7711290b3b201f5db8dfd2d3ff953b00476c8ef5
Đức Nguyễn <75886829+agueroduc01@users.noreply.github.com>
2021-12-10 20:08:40 +0700

    1
    1

---
+++ main.py
@@ -0,0 +1,57 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit
+from PyQt5 import QtWidgets
+#import file
+from ShowHomeAdmin import showHomeAdmin
+from controller.loginController import loginUser, checkSeePassword
+from view.Login import Ui_MainWindow
+from database import myDB
+from messageBox import MBox
+
+
+class MainWindow:
+    # day la test git push
+    def __init__(self):
+
+        self.main_win = QMainWindow()
+        self.uic = Ui_MainWindow()
+        self.uic.setupUi(self.main_win)
+        # code
+        self.uic.login.clicked.connect(self.login)
+        self.uic.password.returnPressed.connect(self.login)
+        self.uic.seePassword.clicked.connect(self.checkPassword)
+
+    def login(self):
+        cur = myDB.cursor()
+        getUname = self.uic.uname.text().strip()
+        getPassword = self.uic.password.text()
+        cur.execute("SELECT * FROM User WHERE mssv=%s AND password=%s",
+                    (getUname, getPassword))
+        result = cur.fetchall()
+        if result[0][-1] == "TEACHER":
+            showHomeAdmin(self)
+        elif result[0][-1] == "STUDENT":
+            print("123")
+        else:
+            MBox(0, 'ERROR', "uname or password wrong", 16)
+
+    def checkPassword(self):
+        if self.uic.seePassword.isChecked():
+            self.uic.password.setEchoMode(QLineEdit.Normal)
+        else:
+            self.uic.password.setEchoMode(QLineEdit.Password)
+
+    def clearContents(self):
+        self.uic.ID_lop.setText("")
+        self.uic.ID_cau_hoi.setText("")
+        self.uic.ten_lop.setText("")
+
+    def show(self):
+        self.main_win.show()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec())
