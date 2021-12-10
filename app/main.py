fab6025a6a156990d3062f44d49e19e633b32c82
huynhngoanhthai <74443669+huynhngoanhthai@users.noreply.github.com>
2021-12-10 14:19:31 +0700

    123213
    222

--- app/main.py
+++ app/main.py
@@ -1,45 +1,40 @@
 import sys
 from PyQt5.QtWidgets import QApplication,QMainWindow,QLineEdit
-from PyQt5 import QtWidgets
-#import file
-from ShowHomeAdmin import showHomeAdmin
-from controller.loginController import loginUser,checkSeePassword 
-from view.Login import Ui_MainWindow
+from Login import Ui_MainWindow
 from database import myDB
-from messageBox import MBox
+
 
 
 class MainWindow:
     def __init__(self):
-        
         self.main_win = QMainWindow()
         self.uic = Ui_MainWindow()
         self.uic.setupUi(self.main_win)
         #code
-        self.uic.login.clicked.connect(self.login)
-        self.uic.password.returnPressed.connect(self.login)
-        self.uic.seePassword.clicked.connect(self.checkPassword)
-    def login(self):
+        self.uic.login.clicked.connect(self.loginUser)
+        self.uic.seePassword.clicked.connect(self.checkSeePassword)
+
+
+    def loginUser(self):
         cur = myDB.cursor()
         getUname = self.uic.uname.text().strip()
         getPassword = self.uic.password.text()
-        cur.execute("SELECT * FROM User WHERE mssv=%s AND password=%s",(getUname,getPassword))
+        
+        cur.execute("SELECT * FROM sinh_vien WHERE mssv=%s AND password=%s",(getUname,getPassword))
+       
         result = cur.fetchall()
-        if result[0][-1]=="ADMIN":
-            showHomeAdmin(self)
-        if result[0][-1]=="STUDENT":
-            print("123")
+        if len(result):
+            print ("Successfully logged in")
+            for x in result:
+                print(x)
         else:
-            MBox(0,'ERROR',"uname or password wrong",16)
-    def checkPassword(self):
+            print("uname or password wrong")
+
+    def checkSeePassword(self):
         if self.uic.seePassword.isChecked():
             self.uic.password.setEchoMode(QLineEdit.Normal)
         else:
             self.uic.password.setEchoMode(QLineEdit.Password)
-    def clearContents(self):
-        self.uic.ID_lop.setText("")
-        self.uic.ID_cau_hoi.setText("")
-        self.uic.ten_lop.setText("")
     def show(self):
         self.main_win.show()
 
