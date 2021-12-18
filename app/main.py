268d611778cd47a5cf8489c566a5cf0073b80be1
huynhngoanhthai <74443669+huynhngoanhthai@users.noreply.github.com>
2021-12-18 16:15:02 +0700

    add question

--- app/main.py
+++ app/main.py
@@ -9,83 +9,6 @@ from database import myDB
 from messageBox import MBox
 
 
-# class MainWindow:
-#     def __init__(self):
-#         self.main_win = QMainWindow()
-#         self.uic = Ui_MainWindow()
-#         self.uic.setupUi(self.main_win)
-#         # code
-#         self.uic.login.clicked.connect(self.login)
-#         self.uic.password.returnPressed.connect(self.login)
-#         self.uic.seePassword.clicked.connect(self.checkPassword)
-#     # Login User
-
-#     def login(self):
-#         try:
-#             cur = myDB.cursor()
-#             getUname = self.uic.uname.text().strip()
-#             getPassword = self.uic.password.text()
-#             cur.execute(
-#                 "SELECT * FROM User WHERE mssv=%s AND password=%s", (getUname, getPassword))
-#             result = cur.fetchall()
-#             if(result == []):
-#                 MBox(0, 'ERROR', "uname or password wrong", 16)
-#             elif result[0][-1] == "TEACHER":
-#                 showHomeAdmin(self)
-#             else:
-#                 # for role = students
-#                 print("123")
-#         except sql.Error as e:
-#             MBox(0, "Error", str(e), 32)
-
-#     def checkPassword(self):
-#         return checkSeePassword(self)
-
-#     # Home Admin
-#     def clearContents(self):
-#         return clearContentsInHomeAdmin(self)
-
-#     def addClass(self):
-#         return addClassInHomeAdmin(self)
-#     # Update Class
-
-#     def updateClass(self):
-#         return updateClassInHomeAdmin(self)
-
-#     def clearContentsUpdateClass(self):
-#         return clearContentsUpdateInHomeAdmin(self)
-#     # func Suggest where press Enter
-
-#     def PressEnterSuggestion(self):
-#         try:
-#             cur = myDB.cursor()
-#             idLop = self.uic.ID_lop_update.text().strip()
-#             query = "SELECT * FROM lop WHERE ID_lop LIKE '%{}%' LIMIT 4".format(
-#                 idLop)
-#             cur.execute(query)
-#             result = cur.fetchall()
-#             self.uic.Suggest.setColumnCount(2)
-#             self.uic.Suggest.setRowCount(5)
-#             columns = 0
-#             for row in result:
-#                 self.uic.Suggest.setItem(columns, 0, QTableWidgetItem(row[0]))
-#                 self.uic.Suggest.setItem(columns, 1, QTableWidgetItem(row[1]))
-#                 columns += 1
-#         except sql.Error as e:
-#             MBox(0, "Error", str(e), 32)
-#     # code Default
-
-#     def show(self):
-#         self.main_win.show()
-
-
-# if __name__ == "__main__":
-#     app = QApplication(sys.argv)
-#     main_win = MainWindow()
-#     main_win.show()
-#     sys.exit(app.exec())
-
-
 def mainUi():
     global ui
     ui = Login.Ui_MainWindow()
@@ -101,18 +24,22 @@ def loginUser():
     try:
         cur = myDB.cursor()
         getUname = ui.uname.text().strip()  # GT
-        print(getUname[0:2])
         getPassword = ui.password.text()
-        cur.execute("SELECT * FROM User WHERE mssv=%s AND password=%s",
+
+        if(getUname[0:2] == "GT"):
+            cur.execute(
+                "SELECT * FROM dmgv WHERE MaGV=%s AND Password=%s", (getUname, getPassword))
+            result = cur.fetchall()
+            if(result == []):
+                return MBox(0, 'ERROR', "uname or password wrong", 16)
+            return showHomeAdmin()
+
+        cur.execute("SELECT * FROM dmsv WHERE MaSV=%s AND Password=%s",
                     (getUname, getPassword))
         result = cur.fetchall()
         if(result == []):
             MBox(0, 'ERROR', "uname or password wrong", 16)
-        elif result[0][-1] == "TEACHER":
-            # showHomeAdmin
-            showHomeAdmin()
         else:
-            # showHomeStudent
             print("123")
     except sql.Error as e:
         MBox(0, "Error", str(e), 32)
@@ -130,90 +57,68 @@ def showHomeAdmin():
     ui = HomeAdmin.Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
+    # default here add
     ui.tab.setCurrentWidget(ui.Add)
-    # add
-    ui.Clear.clicked.connect(clearContents)
-    ui.AddClass.clicked.connect(addClass)
-    # Update
-    ui.UpdateClass.clicked.connect(updateClass)
-    ui.ID_lop_update.returnPressed.connect(PressEnterSuggestion)
-    ui.Clear_update.clicked.connect(clearContentsUpdateClass)
-    # Delete
+    # default ID is
+    cur = myDB.cursor()
+    cur.execute("SELECT max(MaCH) FROM dmch")
+    result = cur.fetchall()[0][0]+1
+    ui.IDAddQuestion.setText(str(result))
+    # event clicked for button
+    ui.AddQuestion.clicked.connect(addQuestion)
+    ui.ClearQuestion.clicked.connect(ClearContentsAddQuestion)
 
 
-def clearContents():
-    ui.ID_lop.setText("")
-    ui.ID_cau_hoi.setText("")
-    ui.ten_lop.setText("")
-
-# -------------------addbbdsuus---------------------------------------------
-
-
-def addClass():
+def addQuestion():
     try:
-        idLop = ui.ID_lop.text().strip()
-        if(idLop == ''):
-            return MBox(0, "Error", "ID lop khong duoc rong", 16)
-        tenLop = ui.ten_lop.text().strip()
-        if(tenLop == ''):
-            return MBox(0, "Error", "ID ten Lop khong duoc rong", 16)
         cur = myDB.cursor()
-        query = "INSERT INTO lop (ID_lop, ten_lop) VALUES (%s, %s)"
-        cur.execute(query, (idLop, tenLop))
+        IDQuestion = ui.IDAddQuestion.text().strip()
+        Question = ui.QLineAQuestion.text().strip()
+        if Question == '':
+            return MBox(0, "Error", "Question not empty", 16)
+        OPA = ui.QLineAOPA.text().strip()
+        if OPA == '':
+            return MBox(0, "Error", "not empty", 16)
+        OPB = ui.QLineAOPB.text().strip()
+        if OPB == '':
+            return MBox(0, "Error", "not empty", 16)
+        OPC = ui.QLineAOPC.text().strip()
+        if OPC == '':
+            return MBox(0, "Error", "not empty", 16)
+        OPD = ui.QLineAOPD.text().strip()
+        if OPD == '':
+            return MBox(0, "Error", "not empty", 16)
+        Answer = ui.QLineAAnswer.text().strip()
+        if Answer == '':
+            return MBox(0, "Error", "not empty", 16)
+        MaMH = ui.QLineAMaMH.text().strip()
+        if MaMH == '':
+            return MBox(0, "Error", "not empty", 16)
+
+        query = "INSERT INTO dmch (MaCH, CauHoi, CauA,CauB,CauC,CauD,DapAn,MaMH) VALUES (%s, %s,%s,%s, %s,%s,%s, %s)"
+        cur.execute(query, (IDQuestion, Question,
+                    OPA, OPB, OPC, OPD, Answer, MaMH))
+        MBox(0, "Successfully", "Successfully", 32)
         myDB.commit()
+        showHomeAdmin()
     except sql.Error as e:
-        MBox(0, "Error", str(e), 32)
+        MBox(0, "Error", str(e), 16)
 
 
-def clearContentsUpdateClass():
-    ui.ID_lop_update.setText("")
-    ui.ten_lop_update.setText("")
-    # comment test
-
-
-def updateClass():
-    try:
-        idLop = ui.ID_lop_update.text().strip()
-        if(idLop == ''):
-            return MBox(0, "Error", "ID lop khong duoc rong", 16)
-        tenLop = ui.ten_lop_update.text()
-        if(tenLop == ''):
-            return MBox(0, "Error", "ten Lop khong duoc rong", 16)
-        cur = myDB.cursor()
-        query = "UPDATE lop SET ten_lop = '{}' WHERE ID_lop = '{}'".format(
-            tenLop, idLop)
-
-        cur.execute(query)
-        MBox(0, "Successfully",
-             "you updated ID_lop = {}, ten_lop = {}".format(idLop, tenLop), 64)
-        myDB.commit()
-        clearContentsUpdateClass()
-    except sql.Error as e:
-        MBox(0, "Error", str(e), 32)
-
-
-def PressEnterSuggestion():
-    try:
-        cur = myDB.cursor()
-        idLop = ui.ID_lop_update.text().strip().upper()
-        query = "SELECT * FROM lop WHERE ID_lop LIKE '%{}%' LIMIT 4".format(
-            idLop)
-        cur.execute(query)
-        result = cur.fetchall()
-        ui.Suggest.setColumnCount(2)
-        ui.Suggest.setRowCount(5)
-        columns = 0
-        for row in result:
-            ui.Suggest.setItem(columns, 0, QTableWidgetItem(row[0]))
-            ui.Suggest.setItem(columns, 1, QTableWidgetItem(row[1]))
-            columns += 1
-    except sql.Error as e:
-        MBox(0, "Error", str(e), 32)
+def ClearContentsAddQuestion():
+    ui.QLineAQuestion.setText("")
+    ui.QLineAOPA.setText("")
+    ui.QLineAOPB.setText("")
+    ui.QLineAOPC.setText("")
+    ui.QLineAOPD.setText("")
+    ui.QLineAAnswer.setText("")
+    ui.QLineAMaMH.setText("")
 
 
 if __name__ == "__main__":
     ui = ''
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    mainUi()
+    # mainUi()
+    showHomeAdmin()
     sys.exit(app.exec())
