04156b35b48f999198f44d8ccdcaab0c8797a7fb
huynhngoanhthai <74443669+huynhngoanhthai@users.noreply.github.com>
2021-12-18 20:03:54 +0700

    Updated Question

--- app/main.py
+++ app/main.py
@@ -64,9 +64,14 @@ def showHomeAdmin():
     cur.execute("SELECT max(MaCH) FROM dmch")
     result = cur.fetchall()[0][0]+1
     ui.IDAddQuestion.setText(str(result))
-    # event clicked for button
+    # event clicked for button in add
+    ui.QLineAMaMH.returnPressed.connect(addQuestion)
     ui.AddQuestion.clicked.connect(addQuestion)
     ui.ClearQuestion.clicked.connect(ClearContentsAddQuestion)
+    # event clicked for button in Update
+    ui.ClearUpdate.clicked.connect(ClearContentsUpdateQuestion)
+    ui.QLineUIDCauHoi.returnPressed.connect(SuggestUpdateQuestion)
+    ui.UpdateQuestion.clicked.connect(updateQuestion)
 
 
 def addQuestion():
@@ -94,7 +99,6 @@ def addQuestion():
         MaMH = ui.QLineAMaMH.text().strip()
         if MaMH == '':
             return MBox(0, "Error", "not empty", 16)
-
         query = "INSERT INTO dmch (MaCH, CauHoi, CauA,CauB,CauC,CauD,DapAn,MaMH) VALUES (%s, %s,%s,%s, %s,%s,%s, %s)"
         cur.execute(query, (IDQuestion, Question,
                     OPA, OPB, OPC, OPD, Answer, MaMH))
@@ -106,6 +110,7 @@ def addQuestion():
 
 
 def ClearContentsAddQuestion():
+    ui.QLineUIDCauHoi.setText("")
     ui.QLineAQuestion.setText("")
     ui.QLineAOPA.setText("")
     ui.QLineAOPB.setText("")
@@ -113,6 +118,92 @@ def ClearContentsAddQuestion():
     ui.QLineAOPD.setText("")
     ui.QLineAAnswer.setText("")
     ui.QLineAMaMH.setText("")
+
+
+def ClearContentsUpdateQuestion():
+    ui.QLineUIDCauHoi.clear()
+    ui.QLineUQuestion.clear()
+    ui.QLineUOPA.clear()
+    ui.QLineUOPB.clear()
+    ui.QLineUOPC.clear()
+    ui.QLineUOPD.clear()
+    ui.QLineUAnswer.clear()
+    ui.QLineUMaMH.clear()
+    ui.QLineUIDCauHoi.setDisabled(False)
+
+
+def SuggestUpdateQuestion():
+    try:
+        cur = myDB.cursor()
+        IDQuestion = ui.QLineUIDCauHoi.text().strip()
+        query = "SELECT * FROM dmch WHERE MaCH LIKE '%{}%' LIMIT 10;".format(
+            IDQuestion)
+        cur.execute(query)
+        result = cur.fetchall()
+        if len(result) == 1:
+            ui.QLineUIDCauHoi.setText(str(result[0][0]))
+            ui.QLineUQuestion.setText(result[0][1])
+            ui.QLineUOPA.setText(result[0][2])
+            ui.QLineUOPB.setText(result[0][3])
+            ui.QLineUOPC.setText(result[0][4])
+            ui.QLineUOPD.setText(result[0][5])
+            ui.QLineUAnswer.setText(result[0][6])
+            ui.QLineUMaMH.setText(result[0][7])
+            ui.QLineUIDCauHoi.setDisabled(True)
+        ui.QTableUpdate.setColumnCount(8)
+        ui.QTableUpdate.setRowCount(10)
+        columns = 0
+        for row in result:
+            ui.QTableUpdate.setItem(columns, 0, QTableWidgetItem(str(row[0])))
+            ui.QTableUpdate.setItem(columns, 1, QTableWidgetItem(row[1]))
+            ui.QTableUpdate.setItem(columns, 2, QTableWidgetItem(row[2]))
+            ui.QTableUpdate.setItem(columns, 3, QTableWidgetItem(row[3]))
+            ui.QTableUpdate.setItem(columns, 4, QTableWidgetItem(row[4]))
+            ui.QTableUpdate.setItem(columns, 5, QTableWidgetItem(row[5]))
+            ui.QTableUpdate.setItem(columns, 6, QTableWidgetItem(row[6]))
+            ui.QTableUpdate.setItem(columns, 7, QTableWidgetItem(row[7]))
+            columns += 1
+
+    except sql.Error as e:
+        MBox(0, "Error", str(e), 16)
+
+
+def updateQuestion():
+    try:
+        cur = myDB.cursor()
+        IDQuestion = ui.QLineUIDCauHoi.text().strip()
+        if IDQuestion == '':
+            return MBox(0, "Error", "not empty", 16)
+        Question = ui.QLineUQuestion.text().strip()
+        if Question == '':
+            return MBox(0, "Error", "Question not empty", 16)
+        OPA = ui.QLineUOPA.text().strip()
+        if OPA == '':
+            return MBox(0, "Error", "not empty", 16)
+        OPB = ui.QLineUOPB.text().strip()
+        if OPB == '':
+            return MBox(0, "Error", "not empty", 16)
+        OPC = ui.QLineUOPC.text().strip()
+        if OPC == '':
+            return MBox(0, "Error", "not empty", 16)
+        OPD = ui.QLineUOPD.text().strip()
+        if OPD == '':
+            return MBox(0, "Error", "not empty", 16)
+        Answer = ui.QLineUAnswer.text().strip()
+        if Answer == '':
+            return MBox(0, "Error", "not empty", 16)
+        MaMH = ui.QLineUMaMH.text().strip()
+        if MaMH == '':
+            return MBox(0, "Error", "not empty", 16)
+        query = "UPDATE dmch SET CauHoi=%s,CauA=%s,CauB=%s,CauC=%s,CauD=%s,DapAn=%s,MaMH=%s WHERE MaCH=%s"
+        cur.execute(query, (Question,
+                    OPA, OPB, OPC, OPD, Answer, MaMH, IDQuestion))
+        MBox(0, "Successfully", "Successfully", 32)
+        myDB.commit()
+        ClearContentsUpdateQuestion()
+
+    except sql.Error as e:
+        MBox(0, "Error", str(e), 16)
 
 
 if __name__ == "__main__":
